0ce4d6d3cf2218d4cd50fa6ea54331f33305853d
Bray <braydickey@gmail.com>
2026-04-02 21:09:46 -0700

    Initial commit

---
+++ src/main/java/bray/palpatch/PalPatchClient.java
@@ -0,0 +1,10 @@
+package bray.palpatch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PalPatchClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/bray/palpatch/PalPatchDataGenerator.java
@@ -0,0 +1,11 @@
+package bray.palpatch;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class PalPatchDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
